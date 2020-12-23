0646cb9d9da67d3dd21ac794e0b7cd4397a16d2d
Lucas Carvalho Machado <lucascarvalhouibai@gmail.com>
2020-12-23 07:42:15 -0300

    Creating exchange controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'exchanges/index'
-  get 'exchanges/convert'
+  root 'exchanges#index'
+  get 'convert', to:'exchanges#convert'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
